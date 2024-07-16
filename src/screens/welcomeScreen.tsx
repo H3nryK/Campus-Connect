e5ba7efe5e03f2b5ddd67420e942edb1fa5d3d5f
H3nryK <henrykimani89@gmail.com>
2024-07-16 11:45:13 +0300

    init

---
+++ src/screens/welcomeScreen.tsx
@@ -0,0 +1,14 @@
+import React from 'react';
+import {View, Text, TouchableOpacity, StyleSheet, Button} from 'react-native';
+
+export default function welcomeScreen({navigation}) {
+  return (
+    <View style={}>
+      <Text> Welcome to Connect Buddy</Text>
+    </View>
+  );
+}
+
+const styles = StyleSheet.create({
+  container: {},
+});
